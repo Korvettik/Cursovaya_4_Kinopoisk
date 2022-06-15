d255a1d4994f0ebce4a62de61e58c035de9dc285
Ilya <korvet1988@yandex.ru>
2022-06-15 21:58:33 +0300

    dao/model

---
+++ dao/model/users.py
@@ -0,0 +1,24 @@
+from setup_db import db
+from marshmallow import Schema, fields
+import hashlib
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String)
+    password = db.Column(db.String)
+    name = db.Column(db.String)
+    surname = db.Column(db.String)
+    favorite_genre = db.Column(db.String)
+
+    # def get_hash(self):
+    #     return hashlib.md5(self.password.encode('utf-8')).hexdigest()
+
+
+class UserSchema(Schema):
+    id = fields.Int()
+    email = fields.Str()
+    password = fields.Str()
+    name = fields.Str()
+    surname = fields.Str()
+    favorite_genre = fields.Str()
